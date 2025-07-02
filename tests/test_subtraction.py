b6a27758b38cb15f1b774f374d2a6958445894bb
kircheis <kerniz@nate.com>
2025-07-02 15:44:19 +0900

    Initial commit

---
+++ tests/test_subtraction.py
@@ -0,0 +1,7 @@
+from kb25pkuser18.subtraction import subtract
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(1, 1) == 0
+    assert subtract(-1, -1) == 0
+    assert subtract(0, 1) == -1
